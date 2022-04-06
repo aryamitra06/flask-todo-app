ce5e9d9cb52f4500093cc6da1845ede86ed2340d
Aryamitra Chaudhuri <caryamitra@gmail.com>
2022-04-06 15:09:17 +0530

    fetching todos

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    desc = db.Column(db.String(500), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def root():
+    # todo = Todo(title= "test title", desc= "test desc")
+    # db.session.add(todo)
+    # db.session.commit()
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo = allTodo)
 
-app.run(debug = True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
